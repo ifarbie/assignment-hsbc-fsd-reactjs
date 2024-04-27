8828a1c03d39f67c76ff3b6003af46730fc49580
ifarbie <farizrifkyberliano@gmail.com>
2024-04-28 02:03:04 +0700

    add: add type to useQuery in useFetchPosts hooks

--- src/features/useFetchPosts.ts
+++ src/features/useFetchPosts.ts
@@ -1,11 +1,11 @@
-import usePostStore from "../lib/zustand/usePostStore";
+import usePostStore, { Post } from "../lib/zustand/usePostStore";
 import axiosInstance from "../lib/axios/axios";
 import { useQuery } from "react-query";
 import { useEffect } from "react";
 
 const useFetchPosts = () => {
     const { setPosts, posts } = usePostStore();
-    const results = useQuery({
+    const results = useQuery<Post[]>({
         queryKey: ["posts"],
         queryFn: () => {
             return axiosInstance.get("/posts?_limit=5").then((res) => res.data);
@@ -15,7 +15,7 @@ const useFetchPosts = () => {
     useEffect(() => {
         if (results.data) setPosts(results.data);
     }, [results.data, setPosts]);
-
+    
     return { ...results, postsList: posts };
 };
 
